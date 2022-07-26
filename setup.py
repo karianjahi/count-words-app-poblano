20144de16a68431f70a1a35a77a1274be6dbc165
joseph <61469887+karianjahi@users.noreply.github.com>
2022-07-26 16:00:07 +0200

    Add requirements and change package name

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 import os
 
 setup(
-    name="count-words-app-poblano",  # name of your package
+    name="count_words_app_poblano",  # name of your package
     version="0.0.1",
     description="a useful package",
     author="nlpoblano",
     author_email="pw@spiced-academy.com",
-    packages=["count-words-app-poblano"],  # same as name
+    packages=["count_words_app_poblano"],  # same as name
     url="git@github.com:karianjahi/count-words-app-poblano.git", #if you have a github repo
     license="MIT",
     classifiers=[
